db666cc603ee9a55fe5a40222eeb1671b1ec84ca
CMitropoulos <chris.mitropoulos@gmail.com>
2017-02-17 11:49:25 -0500

    add number generator code and random numbers generated that will be used for input in the project code

--- code/numberGenerator.c
+++ code/numberGenerator.c
@@ -3,11 +3,84 @@
 
 
 
-main() {
+int main(int argc, char *argv[]) {
    FILE *fp;
-
-   fp = fopen("/input/100numbers.txt", "w");
-   fprintf(fp, "This is testing for fprintf...\n");
-   fputs("This is testing for fputs...\n", fp);
+   int r=0;
+ //WRITE 10numbers
+   fp = fopen("input/10Numbers.txt", "w");
+   srand(time(NULL));
+   if(fp == NULL){
+   	fprintf(stderr, "Cannot open file");
+   } 
+   else{ //this is where we write the random numbers
+   	for(int i=0;i<10;i++){
+   		r = rand()%10000;
+		fprintf(fp, "%d \n", r );
+   	}    	
+   } 
+   
    fclose(fp);
+
+
+ //WRITE 100numbers
+   fp = fopen("input/100Numbers.txt", "w");
+   if(fp == NULL){
+   	fprintf(stderr, "Cannot open file");
+   }
+   else{ //this is where we write the random numbers
+   	for(int i=0;i<100;i++){
+   		r = rand()%100000;
+		fprintf(fp, "%d \n", r );
+   	}   
+	 }
+   fclose(fp);
+
+//WRITE 1knumbers
+   fp = fopen("input/1kNumbers.txt", "w");
+   if(fp == NULL){
+   	fprintf(stderr, "Cannot open file");
+   }
+else{ //this is where we write the random numbers
+   	for(int i=0;i<1000;i++){
+   		r = rand()%100000;
+		fprintf(fp, "%d \n", r );
+   	 }  
+	}    
+   fclose(fp);
+
+ //WRITE 10knumbers
+   fp = fopen("input/10kNumbers.txt", "w");
+   if(fp == NULL){
+   	fprintf(stderr, "Cannot open file");
+   }
+else{ //this is where we write the random numbers
+   	for(int i=0;i<10000;i++){
+   		r = rand()%100000;
+		fprintf(fp, "%d \n", r );
+   		}
+   }      
+   fclose(fp);
+
+ //WRITE 100knumbers
+   fp = fopen("input/100kNumbers.txt", "w");
+   if(fp == NULL){
+   	fprintf(stderr, "Cannot open file");
+   }
+else{ //this is where we write the random numbers
+   	for(int i=0;i<100000;i++){
+   		r = rand()%100000;
+		fprintf(fp, "%d \n", r );
+   	}      
+   }
+   fclose(fp);
+
+
+
+
+
+
+
+
+
+   return 0;
 }
