e46d0c9e70c92ce311d10fc45d350bce6fe6ac24
CMitropoulos <chris.mitropoulos@gmail.com>
2017-03-22 20:42:10 -0400

    first commit for HW2

---
+++ HW2/problem1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+
+	//read user input
+	int n;
+	scanf("%d", &n);
+
+	int *primeArray;
+
+	//Allocate memory for the primeArray and initialize
+	//all values to 1
+	primeArray =(int*)malloc(sizeof(int)*(n+1));
+	for(int i=0;i++;i<n+1)
+		primeArray[i]=1;
+
+	//Create thread to find all the prime numbers
+
+	//Create thread to find the subset of the prime numbers
+	//that the number that is derived by 
+	//reversing the digits is also prime 
+	
+
+	
+	return 0;
+}
