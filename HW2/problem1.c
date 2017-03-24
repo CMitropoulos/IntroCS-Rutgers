1f776a58c25095d73ca87be08a54c6ab05b8719f
CMitropoulos <chris.mitropoulos@gmail.com>
2017-03-23 22:21:16 -0400

    first working thread calling and argument passing code. Need to add the logic for the prime numbers

--- HW2/problem1.c
+++ HW2/problem1.c
@@ -3,22 +3,25 @@
 #include <pthread.h>
 #define NUM_THREADS  2
 
-struct thread_date{
+struct thread_data{
 	int thread_id;
-	int *array
-}
+	int *array;
+};
 
 struct thread_data thread_data_array[NUM_THREADS];
 
 
 void *FindPrime(void *threadData){
-	struct thread_data
-	{
-		/* data */
-	};
-	printf("Thread %ld done. Result = %e\n",tid, result);
-    pthread_exit((void*) t);
+	struct thread_data *my_data;
+	long tid;
+
+
+	printf("Array element%d\n", my_data->array[0] );
+	printf("Thread %ld done.\n",tid);
+    pthread_exit((void*) threadData);
 }
+
+
 int main(int argc, char const *argv[])
 {
 
@@ -27,24 +30,27 @@ int main(int argc, char const *argv[])
 	scanf("%d", &n);
 
 	int *primeArray;
-	pthread_t threads[NUM_THREADS];
-	pthread_attr_t attr;
-	/* Initialize and set thread detached attribute */
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE)
-	int rc;
 	//Allocate memory for the primeArray and initialize
 	//all values to 1
 	primeArray =(int*)malloc(sizeof(int)*(n+1));
 	for(int i=0;i++;i<n+1)
 		primeArray[i]=1;
 
+	void *status;
+	pthread_t threads[NUM_THREADS];
+	pthread_attr_t attr;
+	/* Initialize and set thread detached attribute */
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	
+	
+
 	//Create thread to find all the prime numbers
 	//set the arguments we will pass to the thread
 	thread_data_array[0].thread_id = 0;
 	thread_data_array[0].array = primeArray;
 
-	rc = pthread_create(&threads[0], &attr, FindPrime, (void*)&thread_data_array[0])
+	int rc = pthread_create(&threads[0], &attr, FindPrime, (void*) &thread_data_array[0]);
        if (rc){
           printf("ERROR; return code from pthread_create() is %d\n", rc);
           exit(-1);
@@ -52,14 +58,13 @@ int main(int argc, char const *argv[])
 
    /* Free attribute and wait for the other threads */
     pthread_attr_destroy(&attr);
-    rc = pthread_join(thread[0], &status);
+    rc = pthread_join(threads[0], &status);
 	if (rc) {
           printf("ERROR; return code from pthread_join() is %d\n", rc);
           exit(-1);
           }
-       printf("Main: completed join with thread %ld having a status   
-             of %ld\n",t,(long)status);
-       }
+       printf("Main: completed join with thread ");
+       
 
 
 
